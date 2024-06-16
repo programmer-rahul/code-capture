53f719ee7fc7157675062b5f0c1d957deeeab4a0
Rahul <7678478061k@gmail.com>
2024-06-16 20:06:22 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "CodeCapture - Beautiful Code Snippets Made Easy",
+  description:
+    "Create and customize stunning images of your code with CodeCapture. Choose from diverse themes and export options to share your code in style.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
